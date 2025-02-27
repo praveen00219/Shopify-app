fe7c390cdf15ffa83fe9dd54ba49be41c528ba2c
praveen00219 <paru2192000@gmail.com>
2025-02-27 15:21:26 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://shopify-app-lb46.onrender.com",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
