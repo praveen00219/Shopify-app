107f01d51a5916eceaa4fc8aa1845ca7a490cc90
praveen00219 <paru2192000@gmail.com>
2025-02-27 14:32:56 +0530

    updated with backed deployed url

--- client/src/components/admin-view/image-upload.jsx
+++ client/src/components/admin-view/image-upload.jsx
@@ -50,7 +50,7 @@ function ProductImageUpload({
     const data = new FormData();
     data.append("my_file", imageFile);
     const response = await axios.post(
-      "http://localhost:5000/api/admin/products/upload-image",
+      "https://shopify-backend-app.onrender.com/api/admin/products/upload-image",
       data
     );
 
